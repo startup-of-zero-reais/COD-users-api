6871f5fc0cd5e74e9b33087db3c07fdd53d8cc16
Jean <jean.carlosmolossi@gmail.com>
2021-11-15 18:56:33 -0300

    feat: adicionadas tags de banco e validacao

--- domain/entities/user.go
+++ domain/entities/user.go
@@ -6,15 +6,21 @@ type (
 	UserType string
 
 	User struct {
-		ID        string    `json:"id"`
-		Name      string    `json:"name"`
-		Lastname  string    `json:"lastname"`
-		Email     string    `json:"email"`
-		Type      UserType  `json:"user_type"`
-		CreatedAt time.Time `json:"created_at"`
-		UpdatedAt time.Time `json:"updated_at"`
+		ID        string    `json:"id" gorm:"primaryKey;user_id;type:varchar(36);"`
+		Name      string    `json:"name" gorm:"name" validate:"required"`
+		Lastname  string    `json:"lastname" gorm:"lastname" validate:"required"`
+		Email     string    `json:"email" gorm:"email;unique" validate:"required,email"`
+		Type      UserType  `json:"user_type" gorm:"user_type"`
+		CreatedAt time.Time `json:"created_at" gorm:"created_at;autoCreateTime"`
+		UpdatedAt time.Time `json:"updated_at" gorm:"updated_at;autoUpdateTime"`
 	}
 )
+
+func (u *User) New() *User {
+	return &User{
+		Type: student,
+	}
+}
 
 const (
 	student = UserType("student")
