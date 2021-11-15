1d089175eece6f9012de36269ebe3acb3e908422
Jean <jean.carlosmolossi@gmail.com>
2021-11-15 18:57:54 -0300

    feat: adicionado load de .env

--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
 
-import "github.com/startup-of-zero-reais/COD-users-api/adapters/http/server"
+import (
+	"github.com/startup-of-zero-reais/COD-users-api/adapters/http/server"
+	"log"
+)
+import "github.com/joho/godotenv"
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
 
 func main() {
 	app := server.NewApplication()
