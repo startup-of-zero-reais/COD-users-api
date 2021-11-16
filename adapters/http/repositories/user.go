1312d22aa2f7365fd3ad1c5f18fb4a8d15a19729
Jean <jean.carlosmolossi@gmail.com>
2021-11-15 22:55:05 -0300

    feat: criado repository de user

---
+++ adapters/http/repositories/user.go
@@ -0,0 +1,44 @@
+package repositories
+
+import (
+	"github.com/startup-of-zero-reais/COD-users-api/adapters/http/database"
+	"github.com/startup-of-zero-reais/COD-users-api/domain/entities"
+)
+
+type (
+	User struct {
+		db *database.Database
+	}
+)
+
+func NewUser(db *database.Database) *User {
+	return &User{
+		db: db,
+	}
+}
+
+func (u *User) Get(ids []string, limit uint, offset uint) []entities.User {
+	var users []entities.User
+
+	u.db.Conn.Find(&users, ids).Limit(int(limit)).Offset(int(offset))
+
+	return users
+}
+
+func (u *User) Search(search map[string]interface{}) []entities.User {
+	return nil
+}
+
+func (u *User) Save(user *entities.User) *entities.User {
+	if user != nil {
+		u.db.Conn.Where("id = ?", user.ID).Updates(user)
+	} else {
+		u.db.Conn.Create(user)
+	}
+
+	return user
+}
+
+func (u *User) Delete(id string) *entities.User {
+	return nil
+}
