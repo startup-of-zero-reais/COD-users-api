e93fca170541510927f8d0dc45e466bcc78a49ad
Jean <jean.carlosmolossi@gmail.com>
2021-11-15 22:56:36 -0300

    refactor: ajustes no contrato de servico

--- domain/ports/services/services.go
+++ domain/ports/services/services.go
@@ -4,7 +4,7 @@ import "github.com/startup-of-zero-reais/COD-users-api/domain/entities"
 
 type (
 	UserService interface {
-		List(query map[string]interface{}, page uint, perPage uint) []*entities.User
+		List(ids []string, page uint, perPage uint) []entities.User
 		Get(id string) *entities.User
 		Create(user *entities.User) *entities.User
 		Update(id string, user *entities.User) *entities.User
