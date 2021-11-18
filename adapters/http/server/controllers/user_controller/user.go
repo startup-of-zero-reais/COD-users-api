37f50ef768b785ceb4a714cda7cc33a04ef4f816
Jean <jean.carlosmolossi@gmail.com>
2021-11-17 22:50:16 -0300

    refactor: adicionado paginator ao controller de usuário

--- adapters/http/server/controllers/user_controller/user.go
+++ adapters/http/server/controllers/user_controller/user.go
@@ -3,9 +3,11 @@ package user_controller
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/startup-of-zero-reais/COD-users-api/adapters/http/database"
+	paginatorsAdapter "github.com/startup-of-zero-reais/COD-users-api/adapters/http/paginators"
 	"github.com/startup-of-zero-reais/COD-users-api/adapters/http/server/controllers/router"
 	servicesAdapter "github.com/startup-of-zero-reais/COD-users-api/adapters/http/services"
 	validatorsAdapter "github.com/startup-of-zero-reais/COD-users-api/adapters/http/validators"
+	"github.com/startup-of-zero-reais/COD-users-api/domain/ports/paginators"
 	"github.com/startup-of-zero-reais/COD-users-api/domain/ports/services"
 	"github.com/startup-of-zero-reais/COD-users-api/domain/ports/validators"
 )
@@ -15,6 +17,7 @@ type (
 		Routes    []*router.Route
 		Service   services.UserService
 		Validator validators.UserValidator
+		Paginator paginators.Pager
 		Group     *echo.Group
 	}
 )
@@ -23,6 +26,7 @@ func New(g *echo.Group, db *database.Database) *User {
 	return &User{
 		Service:   servicesAdapter.NewUser(db),
 		Validator: validatorsAdapter.NewUser(),
+		Paginator: paginatorsAdapter.NewPaginator("/users"),
 		Group:     g,
 	}
 }
@@ -31,6 +35,7 @@ func (u *User) Register() {
 	u.List()
 	u.Create()
 	u.Update()
+	u.Delete()
 
 	for _, route := range u.Routes {
 		route.RegisterRoutes()
