08b2f7b97024cba1cd722ecc5aadb2f7c3a99108
Jean <jean.carlosmolossi@gmail.com>
2021-11-17 22:51:13 -0300

    refactor: adicionados totais aos retornos

--- domain/ports/repositories/repositories.go
+++ domain/ports/repositories/repositories.go
@@ -4,8 +4,8 @@ import "github.com/startup-of-zero-reais/COD-users-api/domain/entities"
 
 type (
 	UserRepository interface {
-		Get(ids []string, limit uint, offset uint) []entities.User
-		Search(search map[string]interface{}) []entities.User
+		Get(ids []string, limit uint, offset uint) ([]entities.User, int)
+		Search(search map[string]interface{}) ([]entities.User, int)
 		Save(user *entities.User) *entities.User
 		Delete(id string) bool
 	}
--- domain/ports/services/services.go
+++ domain/ports/services/services.go
@@ -4,10 +4,10 @@ import "github.com/startup-of-zero-reais/COD-users-api/domain/entities"
 
 type (
 	UserService interface {
-		List(ids []string, page uint, perPage uint) []entities.User
+		List(ids []string, page uint, perPage uint) ([]entities.User, int)
 		Get(id string) *entities.User
 		Create(user *entities.User) (*entities.User, error)
 		Update(id string, user *entities.User) (*entities.User, error)
-		Delete(id string) *entities.User
+		Delete(id string) bool
 	}
 )
