8c6f262f60e9982efa18241eec09ebd304351356
Jean <jean.carlosmolossi@gmail.com>
2021-11-17 22:49:43 -0300

    refactor: rota de listagem adicionada paginação e busca por ids

--- adapters/http/server/controllers/user_controller/list.go
+++ adapters/http/server/controllers/user_controller/list.go
@@ -4,11 +4,39 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/startup-of-zero-reais/COD-users-api/adapters/http/server/controllers/router"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
+const MaxUsrIds = 128
+
+func (u *User) extractSearch(values url.Values) []string {
+	if values.Get("ids") != "" {
+		ids := strings.Split(values.Get("ids"), ",")
+		if len(ids) > MaxUsrIds {
+			return ids[:MaxUsrIds]
+		}
+
+		return ids
+	}
+
+	return []string{}
+}
+
 func (u *User) listHandler(c echo.Context) error {
-	users := u.Service.List([]string{}, 1, 10)
-	return c.JSON(http.StatusOK, users)
+	page, perPage := u.Paginator.GetPagination(c)
+
+	users, total := u.Service.List(
+		u.extractSearch(c.QueryParams()),
+		page,
+		perPage,
+	)
+
+	u.Paginator.Total = uint(total)
+
+	paginated := u.Paginator.Paginate(users)
+
+	return c.JSON(http.StatusOK, paginated)
 }
 
 func (u *User) List() {
