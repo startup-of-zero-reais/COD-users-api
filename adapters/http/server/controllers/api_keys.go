122e9a108c2d9cc4af59ce7c21cf077570579a63
Jean <jean.carlosmolossi@gmail.com>
2021-11-22 21:26:49 -0300

    feat: adicionada rota de api-keys

---
+++ adapters/http/server/controllers/api_keys.go
@@ -0,0 +1,43 @@
+package controllers
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/startup-of-zero-reais/COD-users-api/adapters/http/server/middlewares/x_api_key"
+	"net/http"
+	"os"
+)
+
+func GenKey() (string, func(ctx echo.Context) error, echo.MiddlewareFunc) {
+	auth := x_api_key.NewXApiKey()
+
+	return "/gen-api-keys", func(ctx echo.Context) error {
+		application := ctx.Request().Header.Get("application")
+		type secretBody struct {
+			Secret string `json:"secret"`
+		}
+
+		secret := &secretBody{}
+
+		if err := ctx.Bind(secret); err != nil {
+			return ctx.JSON(http.StatusBadRequest, map[string]string{"message": err.Error()})
+		}
+
+		if sec := getEnv("APP_SECRET", "secret0-123-0z"); secret.Secret != sec {
+			return ctx.JSON(http.StatusForbidden, map[string]string{"message": "Forbidden"})
+		}
+
+		key, err := auth.GenerateApiKey(application)
+		if err != nil {
+			return ctx.JSON(http.StatusBadRequest, map[string]string{"message": err.Error()})
+		}
+
+		return ctx.JSON(http.StatusOK, map[string]string{"message": key, "application": application})
+	}, (func(c echo.HandlerFunc) echo.HandlerFunc)(auth.CheckApplication())
+}
+
+func getEnv(key, _default string) string {
+	if e := os.Getenv(key); e != "" {
+		return e
+	}
+	return _default
+}
