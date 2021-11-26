790f57b9a515f6c6fd2dc6491f21d583a508116e
Jean <jean.carlosmolossi@gmail.com>
2021-11-25 21:14:37 -0300

    docs.: criada documentação de ports de paginação

--- domain/ports/paginators/paginators.go
+++ domain/ports/paginators/paginators.go
@@ -6,12 +6,14 @@ import (
 )
 
 type (
+	// Metadata é a estrutura de metadados na resposta da API
 	Metadata struct {
 		Page    uint `json:"page,omitempty"`
 		PerPage uint `json:"per_page,omitempty"`
 		Total   uint `json:"total,omitempty"`
 	}
 
+	// Links é a estrutura de links na resposta da API
 	Links struct {
 		Next     string `json:"next,omitempty"`
 		Previous string `json:"previous,omitempty"`
@@ -19,17 +21,23 @@ type (
 		Last     string `json:"last,omitempty"`
 	}
 
+	// Paginated é a estrutura final de resposta da API
 	Paginated struct {
 		Data     interface{} `json:"data,omitempty"`
 		Metadata Metadata    `json:"_metadata"`
 		Links    Links       `json:"_links"`
 	}
 
+	// Paginator é a interface para que os recursos tenham os métodos necessários
+	// para gerar paginação de request na API
 	Paginator interface {
 		Paginate(items []interface{ resources.Resource }) *Paginated
 		GetPagination(c echo.Context) (uint, uint)
 	}
 
+	// Pager é a estrutura base de paginação
+	//
+	// BaseURL é a url de base para a referência da paginação
 	Pager struct {
 		BaseURL string
 		PerPage uint
