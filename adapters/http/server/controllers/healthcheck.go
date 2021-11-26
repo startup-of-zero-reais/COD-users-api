ce728da840aa660f4e31f11b003a0ca5f3a18739
Jean <jean.carlosmolossi@gmail.com>
2021-11-25 22:12:18 -0300

    docs.: criada documentação do server para o router de healthcheck da aplicação

--- adapters/http/server/controllers/healthcheck.go
+++ adapters/http/server/controllers/healthcheck.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+// HealthCheck é a rota de verificação de saúde da aplicação
 func HealthCheck() (string, func(ctx echo.Context) error) {
 	return "/healthcheck", func(ctx echo.Context) error {
 		return ctx.JSON(http.StatusOK, map[string]string{"message": "health"})
