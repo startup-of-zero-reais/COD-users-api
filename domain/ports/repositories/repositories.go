1c4d4ecebbcba676057a11670db38a4c68614ba7
Jean <jean.carlosmolossi@gmail.com>
2021-11-25 21:15:58 -0300

    docs.: criada documentação de ports de repositories.go

--- domain/ports/repositories/repositories.go
+++ domain/ports/repositories/repositories.go
@@ -3,6 +3,7 @@ package repositories
 import "github.com/startup-of-zero-reais/COD-users-api/domain/entities"
 
 type (
+	// UserRepository é a ‘interface’ de repositório necessária para usuários
 	UserRepository interface {
 		Get(ids []string, limit uint, offset uint) ([]entities.User, int)
 		Search(search map[string]interface{}) ([]entities.User, int)
@@ -10,6 +11,7 @@ type (
 		Delete(id string) bool
 	}
 
+	// TokenRepository é a ‘interface’ de repositório necessária para tokens
 	TokenRepository interface {
 		Get(id string) *entities.RecoverToken
 		Generate(token, email string) *entities.RecoverToken
