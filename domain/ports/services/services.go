5a507f9b8eb8b85129004f9c9ee0bb0a0ea41ff9
Jean <jean.carlosmolossi@gmail.com>
2021-11-25 21:19:05 -0300

    docs.: criada documentação de ports de services.go

--- domain/ports/services/services.go
+++ domain/ports/services/services.go
@@ -3,6 +3,7 @@ package services
 import "github.com/startup-of-zero-reais/COD-users-api/domain/entities"
 
 type (
+	// UserService 'interface' para que os serviços de usuário funcionem corretamente
 	UserService interface {
 		List(ids []string, page uint, perPage uint) ([]entities.User, int)
 		Get(id string) *entities.User
@@ -11,10 +12,12 @@ type (
 		Delete(id string) bool
 	}
 
+	// AuthService 'interface' para o serviço de autenticação
 	AuthService interface {
 		Get(email string) *entities.User
 	}
 
+	// RecoverAccountService 'interface' para o serviço de recuperação de conta
 	RecoverAccountService interface {
 		SendEmail(email string) bool
 		GetToken(id string) (*entities.RecoverToken, error)
