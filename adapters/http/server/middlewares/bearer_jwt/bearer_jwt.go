0e130afaea8207c606792b0ca5f257148af551fb
Jean <jean.carlosmolossi@gmail.com>
2021-12-03 23:01:19 -0300

    feat: criado jwt decode

--- adapters/http/server/middlewares/bearer_jwt/bearer_jwt.go
+++ adapters/http/server/middlewares/bearer_jwt/bearer_jwt.go
@@ -1,10 +1,14 @@
 package bearer_jwt
 
 import (
+	"errors"
+	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/startup-of-zero-reais/COD-users-api/adapters/http/server/controllers/router"
 	"github.com/startup-of-zero-reais/COD-users-api/adapters/http/services"
+	"github.com/startup-of-zero-reais/COD-users-api/domain/entities"
 	"github.com/startup-of-zero-reais/COD-users-api/domain/utilities"
+	"time"
 )
 
 // JwtHeaderConfig retorna o middleware que valida o jwt de autenticação
@@ -15,4 +19,31 @@ func JwtHeaderConfig() router.MiddlewareHandler {
 	}
 
 	return (router.MiddlewareHandler)(middleware.JWTWithConfig(config))
+}
+
+func JwtDecode(tokenString string) (*entities.User, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte(utilities.GetEnv("APP_SECRET", "tokn-secre7")), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	var user entities.User
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		user.ID = claims["sub"].(string)
+		user.Email = claims["email"].(string)
+		user.Name = claims["name"].(string)
+		user.Lastname = claims["lastname"].(string)
+		user.Type = entities.UserType(claims["user_type"].(string))
+		user.CreatedAt, _ = time.Parse(time.RFC3339, claims["created_at"].(string))
+		user.UpdatedAt, _ = time.Parse(time.RFC3339, claims["updated_at"].(string))
+		user.HideSensitiveFields()
+
+		return &user, nil
+	}
+
+	return nil, errors.New("invalid claim user")
 }
